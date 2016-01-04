85e9fd1576677950ad8086fb48b7b1ba8e143859
Chris Hintz <chrishintz42@gmail.com>
2016-01-04 14:58:56 -0800

    Add controller, routing and view for sign-up

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "/sign-up",                 to: "users#index",      as: :sign_up
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
